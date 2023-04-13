56314e4273e8438583f2ebd94e2c03a2317ac902
erol0742 <erol007442@gmail.com>
2023-04-13 14:54:33 +0300

    CASE10

---
+++ src/test/java/StepDefinitions/StepDefinitions_10_Steps/User_Story_10_EditSteps.java
@@ -0,0 +1,24 @@
+package StepDefinitions.StepDefinitions_10_Steps;
+
+import Pages.DialogContent;
+import Pages.FormContent;
+import Pages.Parent;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class User_Story_10_EditSteps {
+
+    DialogContent dc=new DialogContent();
+    FormContent fm=new FormContent();
+    Parent parent=new Parent();
+
+    @Given("by clicking the Setup, Parameters, Grade Levels options.")
+    public void byClickingTheSetupParametersGradeLevelsOptions() {
+    }
+
+    @When("confirm grade levels")
+    public void confirmGradeLevels() {
+    }
+
+
+}
