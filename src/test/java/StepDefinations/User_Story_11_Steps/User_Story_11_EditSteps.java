b81c42bf5888a29492b5dffe3e6e3611738a97c0
ahmet <ahmetersz@gmail.com>
2023-04-13 04:39:41 +0300

    step11 ok

--- src/test/java/StepDefinations/User_Story_11_Steps/User_Story_11_EditSteps.java
+++ src/test/java/StepDefinations/User_Story_11_Steps/User_Story_11_EditSteps.java
@@ -5,13 +5,16 @@ import Pages.FormContent;
 import Pages.Parent;
 import io.cucumber.java.en.When;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class User_Story_11_EditSteps {
     DialogContent dc=new DialogContent();
     Parent parent=new Parent();
     FormContent fm=new FormContent();
 
     @When("Edit to Discounts")
-    public void editToDiscounts() {
+    public void editToDiscounts() throws AWTException {
         parent.waitEditButtonVerify();
         dc.findAndSend("descriptionInput","Joaquin");
         dc.findAndClick("SEARCHBUTTON");
@@ -20,6 +23,9 @@ public class User_Story_11_EditSteps {
         fm.findAndSend("getDescription","Toni");
         fm.findAndSend("getIntegrationCode","LATO");
         fm.findAndSend("priority","20");
+        Robot rbt = new Robot();
+        rbt.keyPress(KeyEvent.VK_TAB);
+        rbt.keyRelease(KeyEvent.VK_TAB);
         dc.findAndClick("saveButton");
 
     }
